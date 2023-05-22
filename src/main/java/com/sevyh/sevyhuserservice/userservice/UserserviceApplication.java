eb31dd621e4b545e6b80a9dcd3b09db80aa70b91
Jules <jules.houben@live.nl>
2023-05-22 19:43:24 +0200

    Update CORS settings

--- src/main/java/com/sevyh/sevyhuserservice/userservice/UserserviceApplication.java
+++ src/main/java/com/sevyh/sevyhuserservice/userservice/UserserviceApplication.java
@@ -1,13 +1,32 @@
 package com.sevyh.sevyhuserservice.userservice;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(
+        Arrays.asList("GET", "POST", "PUT","DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
 	}
 
 }
--- src/main/java/com/sevyh/sevyhuserservice/userservice/api/UserController.java
+++ src/main/java/com/sevyh/sevyhuserservice/userservice/api/UserController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sevyh.sevyhuserservice.userservice.service.KeycloakService;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/v1/users")
 public class UserController {
     
